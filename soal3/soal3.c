eb07abea3c9bda054e645e0a3536a1b7b7a1af1b
Alberto0150 <61089243+Alberto0150@users.noreply.github.com>
2020-03-13 11:16:38 +0700

    Create soal3.c

---
+++ soal3/soal3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() 
+{
+pid_t child_id;
+int status;
+
+child_id = fork();
+
+if (child_id < 0) 
+	{
+		exit(EXIT_FAILURE); 
+	}
+
+if (child_id == 0) 
+	{
+
+		char *argv[] = {"mkdir", "-p", "/home/alberto/modul2/indomie", NULL};
+		execv("/bin/mkdir", argv);
+	}
+sleep(5);
+child_id = fork();
+if (child_id == 0) 
+	{	
+		char *argv[] = {"mkdir", "-p", "/home/alberto/modul2/sedaap", NULL};
+		execv("/bin/mkdir", argv);
+	}
+
+
+}
