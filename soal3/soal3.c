07e4d4b3471502e7d9dbbb5dfb13d0a434bbed68
Alberto0150 <61089243+Alberto0150@users.noreply.github.com>
2020-03-13 17:37:37 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
+
 
 int main() 
 {
@@ -36,5 +42,38 @@ if(child_id == 0)
 {
 	char *argv[] = {"unzip","jpg.zip",NULL};
 	execv("/usr/bin/unzip", argv);
+}
+
+
+// bagian C
+struct dirent *check;
+DIR* masuk;
+
+masuk = opendir("/home/alberto/modul2/jpg/");
+check = readdir(masuk);
+char nama[100];
+
+while(1)
+{
+	if (check == NULL)
+	{
+		break;
+	}
+	else
+	{
+		strcpy(nama,"/home/alberto/modul2/jpg/");
+		strcat(nama,check->d_name);
+		if(check->d_type==DT_DIR)
+		{
+			char *argv[] = {"mv",nama,"/home/alberto/modul2/indomie",NULL};
+			execv("/bin/mv",argv);
+		}
+		else if (check->d_type==DT_REG)
+		{
+			char *argv[] = {"mv",nama,"/home/alberto/modul2/sedaap",NULL};
+			execv("/bin/mv",argv);
+		}
+
+	}
 }
 }
